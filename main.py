a999bbc8aefeaa9e8dc558afa3b762740e5b9c65
Max2450 <mrg245000@gmail.com>
2025-09-09 10:56:58 -0700

    Added ability to accept user input and exit if none is provided. Also added a check for valid Gemini API Key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 def main():
     print("Hello from aiproject!")
@@ -8,10 +9,23 @@ def main():
     # fetch API key from project root dir and create a Gemini Client using it
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+
+    # check that API_KEY is not empty, if it is, exit.
+    if api_key != False:
+        client = genai.Client(api_key=api_key)
+    else:
+        print("Missing GEMINI_API_KEY")
+        sys.exit(1)
+
+    # check for user inputted prompt, if none exists, raise Exception. 
+    if len(sys.argv) < 2:
+        print("No prompt provided")
+        sys.exit(1)
+    
+    user_input = sys.argv[1]
 
     # send a prompt using gemini 2.0 flash
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=user_input)
     
     # print the response and token usage
     print(response.text)
