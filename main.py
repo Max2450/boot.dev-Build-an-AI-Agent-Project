bfbec135178609aeb2345a7aef5967bd961f29f9
Max2450 <mrg245000@gmail.com>
2025-09-09 11:19:53 -0700

    Added verbosity flag and checks for verbosity. Refined API_KEY checks.

--- main.py
+++ main.py
@@ -2,37 +2,52 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 def main():
-    print("Hello from aiproject!")
+    #print("Hello from aiproject!")
+    #print("=====Printing sys.argv for debugging=====")
+    #print(sys.argv)
     
     # fetch API key from project root dir and create a Gemini Client using it
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
-    # check that API_KEY is not empty, if it is, exit.
-    if api_key != False:
-        client = genai.Client(api_key=api_key)
-    else:
+    # check that API_KEY is not empty or None, if it is, exit.
+    if not api_key:
         print("Missing GEMINI_API_KEY")
         sys.exit(1)
-
+    client = genai.Client(api_key=api_key)
+        
     # check for user inputted prompt, if none exists, raise Exception. 
     if len(sys.argv) < 2:
         print("No prompt provided")
         sys.exit(1)
+
+    # check for verbose flag, set to True if it's in the 2nd index or later
+    verbose = False
+    if len(sys.argv) > 2:
+        if '--verbose' in sys.argv[2:]:
+            verbose = True
     
     user_input = sys.argv[1]
 
-    # send a prompt using gemini 2.0 flash
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=user_input)
+    # create a list of types.content, setting the user's input as the only message for now
+    messages = [types.Content(role="user", parts=[types.Part(text=user_input)])]
+
+    # send a prompt using gemini 2.0 flash, passing in the list of user messages
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     
-    # print the response and token usage
+    # print the response
     print(response.text)
-    prompt_tokens_used = response.usage_metadata.prompt_token_count
-    response_tokens_used = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens_used}")
-    print(f"Response tokens: {response_tokens_used}")
+    
+    # if verbose tag used, print tokens used and prompt used
+    if response and verbose:
+        prompt_tokens_used = response.usage_metadata.prompt_token_count
+        response_tokens_used = response.usage_metadata.candidates_token_count
+        print(f"User prompt: {user_input}")
+        print(f"Prompt tokens: {prompt_tokens_used}")
+        print(f"Response tokens: {response_tokens_used}")
 
 if __name__ == "__main__":
     main()
