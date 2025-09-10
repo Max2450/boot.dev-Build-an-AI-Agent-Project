a03c425a57f66b56e6bd09fd6bb56f8e583cb008
Max2450 <mrg245000@gmail.com>
2025-09-10 10:04:01 -0700

    added comments to list folder contents function for clarity and posterity.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,15 +1,27 @@
 import os
 
+"""List contents of a directory and their sizes and is_dir values, checking that the target dir 
+is inside of the working dir scope and a valid dir."""
 def get_files_info(working_directory, directory="."):
-    # full_path = os.path.join(working_directory, directory)
+    # create normalized absolute paths for working dir and target dir
     abs_work = os.path.normpath(os.path.abspath(working_directory))
     abs_target = os.path.normpath(os.path.abspath(os.path.join(working_directory, directory)))
 
+    # check that the target path starts with the working dir path so that AI is working
+    # in scope, and that target path and working path are not identical (which allows
+    # AI to run this on the working dir without erroring out)
     if not abs_target.startswith(abs_work + os.sep) and abs_target != abs_work:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    # check that the target path is a dir, error out if it is not
     if not os.path.isdir(abs_target):
         return f'Error: "{directory}" is not a directory'
     
+    # try looping through each item in the target dir,
+    # grabbing its name, path, size, is_dir,
+    # and appending them in a formatted string to a list
+    # that is then returned as a string separated by newlines. 
+    # if an Exception is raised, return a formatted error string. 
     try:
         line_list = []
         for item in os.listdir(abs_target):
